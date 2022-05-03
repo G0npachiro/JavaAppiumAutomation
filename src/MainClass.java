624f97af8f53f6d7bcbba520f7083d8dafc2c6e5
sashka9106@gmail.com <Muw7hjci@>
2022-05-03 20:53:36 +0300

    1/2/3 задания

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,16 +1,22 @@
 import org.junit.Test;
 
 public class MainClass {
-   public int getLocalNumber()
-   {
-       return 14;
-   }
+    public int getLocalNumber()
+    {
+        return 14;
+    }
 
-   public int class_number = 20;
+public int class_number = 20;
+    public int getClass_number()
 
-   public int getClass_number()
-   {
-       return class_number;
-   }
+    {
+        return class_number;
+    }
+
+public String class_string = "Hello,world";
+    public String getClass_string()
+    {
+        return class_string;
+    }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,25 +4,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class MainClassTest extends MainClass
-{
+public class MainClassTest extends MainClass {
     @Before
-    public void textStartTest()
-    {
+    public void textStartTest() {
         System.out.println("Начинаем тест");
     }
+
     @After
-    public void textEndTest()
-    {
+    public void textEndTest() {
         System.out.println("Тест окончен");
     }
 
     @Test
-    public void testGetLocalNumber()
-    {
+    public void testGetLocalNumber() {
         int a = getLocalNumber();
-        if (a == 14)
-        {
+        if (a == 14) {
             System.out.println("TestDone");
         } else {
             Assert.fail("FailTest");
@@ -30,15 +26,24 @@ public class MainClassTest extends MainClass
     }
 
     @Test
-    public void testGetClassNumber()
-    {
+
+    public void testGetClassNumber() {
         int a = getClass_number();
-        if (a < 45)
-        {
+        if (a < 45) {
             System.out.println("TestDone");
         } else {
             Assert.fail("FailTest");
         }
 
+    }
+
+    @Test
+    public void testGetClassString() {
+        String b = getClass_string();
+        if (b.contains("Hello")) {
+            System.out.println("TestDone");
+        } else {
+            Assert.fail("FailTest");
+        }
     }
 }
